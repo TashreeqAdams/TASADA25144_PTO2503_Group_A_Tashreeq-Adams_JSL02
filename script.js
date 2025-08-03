f7974d9715efb707c0fe2d1a582742cfd32ff600
Tashreeq Adams <tashreeqadams8@gmail.com>
2025-08-03 19:47:07 +0200

    Task 2 JS Code Added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // First Task
+
 let taskTitle = "";
 while (!taskTitle) {
   taskTitle = prompt("Enter Task 1 title:");
@@ -12,7 +13,7 @@ while (!taskDescription) {
 while (true) {
   taskStatus = prompt("Enter Task 1 Status (todo, doing, done):");
 
-  const statusLower = taskStatus?.toLowerCase();
+  let statusLower = taskStatus?.toLowerCase();
 
   if (statusLower === "done") {
     console.log("Task 1 status: done");
@@ -21,7 +22,33 @@ while (true) {
     console.log("No tasks completed, let's get to work!");
     break;
   } else {
-    console.log("bad");
+    alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
+  }
+}
+
+// Second Task
+let taskTitle2 = "";
+while (!taskTitle2) {
+  taskTitle2 = prompt("Enter Task 2 title:");
+}
+
+let taskDescription2 = "";
+while (!taskDescription2) {
+  taskDescription2 = prompt("Enter Task 2 description:");
+}
+
+while (true) {
+  taskStatus2 = prompt("Enter Task 2 Status (todo, doing, done):");
+
+  let statusLower = taskStatus2?.toLowerCase();
+
+  if (statusLower === "done") {
+    console.log("Task 2 status: done");
+    break;
+  } else if (statusLower === "todo" || statusLower === "doing") {
+    console.log("No tasks completed, let's get to work!");
+    break;
+  } else {
     alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
   }
 }
